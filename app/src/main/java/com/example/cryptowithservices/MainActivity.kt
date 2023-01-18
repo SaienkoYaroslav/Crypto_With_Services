cb630dd4989db1937daa18906c5b96bddfb66ed7
SaienkoYaroslav <saienko.yaroslav01@gmail.com>
2023-01-18 19:56:08 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cryptowithservices/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.cryptowithservices
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.cryptowithservices.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+    }
+
+
+    
+}
