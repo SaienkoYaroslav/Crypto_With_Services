9645a5dfbd0830d70a2a2abae245db34c7a97f24
SaienkoYaroslav <saienko.yaroslav01@gmail.com>
2023-01-23 21:51:20 +0200

    notification

--- app/src/main/java/com/example/cryptowithservices/MainActivity.kt
+++ app/src/main/java/com/example/cryptowithservices/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.cryptowithservices
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 import com.example.cryptowithservices.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -16,8 +21,32 @@ class MainActivity : AppCompatActivity() {
         binding.simpleService.setOnClickListener {
             startService(MyService.newServiceIntent(this, 50))
         }
-
+        binding.foregroundService.setOnClickListener {
+            showNotification()
+        }
     }
 
+    private fun showNotification() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Hui")
+            .setContentText("pyzda rylu")
+            .setSmallIcon(androidx.core.R.drawable.notification_template_icon_bg)
+            .build()
+        notificationManager.notify(1, notification)
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
+    }
 
 }
