445f3cd9ddc39680277d5f9b38ab7f4123824c0a
SaienkoYaroslav <saienko.yaroslav01@gmail.com>
2023-01-24 18:22:53 +0200

    job service

--- app/src/main/java/com/example/cryptowithservices/MainActivity.kt
+++ app/src/main/java/com/example/cryptowithservices/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.example.cryptowithservices
 
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
 import com.example.cryptowithservices.databinding.ActivityMainBinding
 
